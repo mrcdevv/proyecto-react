5fe1c1811b6d7564e2b9b0c6191060e851844f89
mrc <marcoemorales2015@gmail.com>
2024-02-02 23:45:02 -0300

    componente MovieDetail en progreso

---
+++ src/components/MovieDetails.jsx
@@ -0,0 +1,22 @@
+function MovieDetails() {
+
+  return (
+    <div>
+      <div>
+        <img src="" alt="" />
+      </div>
+      <div>
+        <h1></h1>
+        <p><span>Título Original: </span><span></span></p>
+        <p><span>Duración: </span><span></span></p>
+        <p><span>Año: </span><span></span></p>
+        <p><span>Sinópsis: </span><span></span></p>
+        <p><span>Calidad: </span><span></span></p>
+        <p><span>Reparto: </span><span></span></p>
+        <p><span>Reparto: </span><span></span></p>
+      </div>
+    </div>
+  )
+}
+
+export default MovieDetails
