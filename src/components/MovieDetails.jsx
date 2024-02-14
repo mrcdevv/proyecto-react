59f548ff852eabcdf1a24170932a2031891387ef
mrc <marcoemorales2015@gmail.com>
2024-02-13 23:57:43 -0300

    Estilos para MovieDetail en progreso

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -17,14 +17,14 @@ function MovieDetails({ url }) {
         <img src={img_url} alt="" />
       </div>
       <div>
-        <h1>{data.title}</h1>
-        <p><span>Título Original: </span><span>{data.original_title}</span></p>
-        <p><span>Duración: </span><span>1h 20m</span></p>
-        <p><span>Año: </span><span>{data.release_date}</span></p>
-        <p><span>Sinópsis: </span><span>{data.overview}</span></p>
-        <p><span>Calidad: </span><span>Español - HD</span></p>
-        <p><span>Reparto: </span><span> Me, You, They</span></p>
-        <p><span>Popularidad: </span><span>{roundToDecimal(data.vote_average, 2)}/10</span></p>
+        <h1 className="">{data.title}</h1>
+        <p><span className="">Título Original: </span><span className="">{data.original_title}</span></p>
+        <p><span className="">Duración: </span><span className="">1h 20m</span></p>
+        <p><span className="">Año: </span><span className="">{data.release_date}</span></p>
+        <p><span className="">Sinópsis: </span><span className="">{data.overview}</span></p>
+        <p><span className="">Calidad: </span><span className="">Español - HD</span></p>
+        <p><span className="">Reparto: </span><span className=""> Me, You, They</span></p>
+        <p><span className="">Popularidad: </span><span className="">{roundToDecimal(data.vote_average, 2)}/10</span></p>
       </div>
     </div>
   )
