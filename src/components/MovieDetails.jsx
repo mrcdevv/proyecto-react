8c729c029c753e1f5f835907f89d989b9972ac93
mrc <marcoemorales2015@gmail.com>
2024-02-15 23:57:54 -0300

    Estilos para MovieDetail en progreso

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -12,19 +12,19 @@ function MovieDetails({ url }) {
   const img_url = `http://image.tmdb.org/t/p/w342/${data.poster_path}`
 
   return (
-    <div>
+    <div className="flex">
       <div>
-        <img src={img_url} alt="" />
+        <img src={img_url} alt="" className="w-48 h-auto" />
       </div>
-      <div>
-        <h1 className="">{data.title}</h1>
-        <p><span className="">Título Original: </span><span className="">{data.original_title}</span></p>
-        <p><span className="">Duración: </span><span className="">1h 20m</span></p>
-        <p><span className="">Año: </span><span className="">{data.release_date}</span></p>
-        <p><span className="">Sinópsis: </span><span className="">{data.overview}</span></p>
-        <p><span className="">Calidad: </span><span className="">Español - HD</span></p>
-        <p><span className="">Reparto: </span><span className=""> Me, You, They</span></p>
-        <p><span className="">Popularidad: </span><span className="">{roundToDecimal(data.vote_average, 2)}/10</span></p>
+      <div className="ml-4">
+        <h1 className="text-2xl font-bold">{data.title}</h1>
+        <p><span className="font-bold">Título Original: </span><span className="">{data.original_title}</span></p>
+        <p><span className="font-bold">Duración: </span><span className="">1h 20m</span></p>
+        <p><span className="font-bold">Año: </span><span className="">{data.release_date}</span></p>
+        <p><span className="font-bold">Sinópsis: </span><span className="">{data.overview}</span></p>
+        <p><span className="font-bold">Calidad: </span><span className="">Español - HD</span></p>
+        <p><span className="font-bold">Reparto: </span><span className=""> Me, You, They</span></p>
+        <p><span className="font-bold">Popularidad: </span><span className="">{roundToDecimal(data.vote_average, 2)}/10</span></p>
       </div>
     </div>
   )
